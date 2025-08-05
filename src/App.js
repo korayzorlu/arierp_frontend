c14ecb801c8b7e7163274da0e63ddfbec8c93fa8
Duzman46 <furkanduzman46@gmail.com>
2025-08-05 13:58:27 +0300

    #20250805 fix: global settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+//deneme
+
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Landing from './features/layout/pages/Landing.js';
 import Panel from './features/layout/pages/Panel.js';
@@ -99,7 +101,7 @@ export const NumberContext = React.createContext();
 
 function App() {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-  const {user,status,theme,dark,loading} = useSelector((store) => store.auth);
+  const { user, status, theme, dark, loading } = useSelector((store) => store.auth);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -113,21 +115,21 @@ function App() {
   useEffect(() => {
     //fetchTheme();
     dispatch(fetchTheme(theme));
-  }, [dark,dispatch]);
-  
+  }, [dark, dispatch]);
+
   useEffect(() => {
     const fetchData = async () => {
-      try{
+      try {
         await dispatch(fetchUser()).unwrap();
         await dispatch(fetchCSRFToken()).unwrap();
-      }catch(error){}
-      
+      } catch (error) { }
+
       dispatch(setLoading(false));
     };
-    
+
     fetchData();
 
-  },[dispatch]);
+  }, [dispatch]);
 
   //sidebar collapse
   useEffect(() => {
@@ -174,138 +176,138 @@ function App() {
     </ThemeProvider>
   );
 
- 
+
 
   return (
     <ThemeProvider>
-        <div className="App" id='Marswide'>
+      <div className="App" id='Marswide'>
 
-          { user && status
-          ? 
-            <>
-              <Routes>
+        {user && status
+          ?
+          <>
+            <Routes>
 
-                <Route exact path='/' element={<Panel></Panel>}>
-                  <Route index element={<Dashboard></Dashboard>}></Route>
-                  <Route path='profile/:username' element={<Profile></Profile>}></Route>
-                  <Route path='notification' element={<Notification></Notification>}></Route>
+              <Route exact path='/' element={<Panel></Panel>}>
+                <Route index element={<Dashboard></Dashboard>}></Route>
+                <Route path='profile/:username' element={<Profile></Profile>}></Route>
+                <Route path='notification' element={<Notification></Notification>}></Route>
 
-                  <Route path='settings' element={<Settings></Settings>}>
-                    <Route path='auth' element={<AuthSettingsLinks></AuthSettingsLinks>}></Route>
-                    <Route path='auth/profile' element={<ProfileSettings></ProfileSettings>}></Route>
-                    <Route path='auth/personal' element={<PersonalSettings></PersonalSettings>}></Route>
-                    <Route path='auth/email' element={<EmailSettings></EmailSettings>}></Route>
-                    <Route path='auth/phone-number' element={<PhoneNumberSettings></PhoneNumberSettings>}></Route>
-                    <Route path='auth/password-reset' element={<PasswordReset></PasswordReset>}></Route>
-                    <Route path='organization' element={<OrganizationSettingsLink></OrganizationSettingsLink>}></Route>
-                    <Route path='organization/currency' element={<CurrencySettings></CurrencySettings>}></Route>
-                  </Route>
-                  <Route path='phone-number-verify' element={<PhoneNumberVerify></PhoneNumberVerify>}></Route>
+                <Route path='settings' element={<Settings></Settings>}>
+                  <Route path='auth' element={<AuthSettingsLinks></AuthSettingsLinks>}></Route>
+                  <Route path='auth/profile' element={<ProfileSettings></ProfileSettings>}></Route>
+                  <Route path='auth/personal' element={<PersonalSettings></PersonalSettings>}></Route>
+                  <Route path='auth/email' element={<EmailSettings></EmailSettings>}></Route>
+                  <Route path='auth/phone-number' element={<PhoneNumberSettings></PhoneNumberSettings>}></Route>
+                  <Route path='auth/password-reset' element={<PasswordReset></PasswordReset>}></Route>
+                  <Route path='organization' element={<OrganizationSettingsLink></OrganizationSettingsLink>}></Route>
+                  <Route path='organization/currency' element={<CurrencySettings></CurrencySettings>}></Route>
+                </Route>
+                <Route path='phone-number-verify' element={<PhoneNumberVerify></PhoneNumberVerify>}></Route>
 
-                  <Route path='/companies' element={<Company></Company>}></Route>
-                  <Route path='/companies/add-company' element={<AddCompany></AddCompany>}></Route>
-                  <Route path='/companies/update/:name' element={<UpdateCompany></UpdateCompany>}></Route>
-                  <Route path='/invitations' element={<Invitations></Invitations>}></Route>
+                <Route path='/companies' element={<Company></Company>}></Route>
+                <Route path='/companies/add-company' element={<AddCompany></AddCompany>}></Route>
+                <Route path='/companies/update/:name' element={<UpdateCompany></UpdateCompany>}></Route>
+                <Route path='/invitations' element={<Invitations></Invitations>}></Route>
 
-                  <Route path='/sectors' element={<Sectors></Sectors>}></Route>
-                  <Route path='/partners/add-sector' element={<AddSector></AddSector>}></Route>
-                  <Route path='/partners/update-sector/:uuid' element={<UpdateSector></UpdateSector>}></Route>
+                <Route path='/sectors' element={<Sectors></Sectors>}></Route>
+                <Route path='/partners/add-sector' element={<AddSector></AddSector>}></Route>
+                <Route path='/partners/update-sector/:uuid' element={<UpdateSector></UpdateSector>}></Route>
 
-                  <Route path='/partners' element={<Partners></Partners>}></Route>
-                  <Route path='/partners/add-partner' element={<AddPartner></AddPartner>}></Route>
-                  <Route path='/partners/update/:uuid' element={<UpdatePartner></UpdatePartner>}></Route>
+                <Route path='/partners' element={<Partners></Partners>}></Route>
+                <Route path='/partners/add-partner' element={<AddPartner></AddPartner>}></Route>
+                <Route path='/partners/update/:uuid' element={<UpdatePartner></UpdatePartner>}></Route>
 
-                  <Route path='/categories' element={<Categories></Categories>}>
-                    <Route path='add-category' element={<AddCategory></AddCategory>}></Route>
-                    <Route path='update/:uuid' element={<UpdateCategory></UpdateCategory>}></Route>
-                  </Route>
-                  
-                  <Route path='/products/update/:uuid' element={<UpdateCategory></UpdateCategory>}></Route>
-
-                  <Route path='/contracts' element={<Contracts></Contracts>}></Route>
-                  <Route path='/contract-payments' element={<ContractPayments></ContractPayments>}></Route>
-
-                  <Route path='/leases' element={<Leases></Leases>}></Route>
-                  <Route path='/leasing/add-lease' element={<AddLease></AddLease>}></Route>
-                  <Route path='/leasing/update/:uuid/:contract_code' element={<UpdateLease></UpdateLease>}></Route>
-                  <Route path='/installments' element={<Installment></Installment>}></Route>
-                  <Route path='/collections' element={<Collections></Collections>}></Route>
-
-                  <Route path='/overdue-leases' element={<OverdueLeases></OverdueLeases>}></Route>
-                  <Route path='/risk-partners' element={<RiskPartners></RiskPartners>}></Route>
-                  <Route path='/kdv-risk-partners' element={<RiskPartnersKDV></RiskPartnersKDV>}></Route>
-                  <Route path='/to-warned-risk-partners' element={<ToWarnedRiskPartners></ToWarnedRiskPartners>}></Route>
-                  <Route path='/warned-risk-partners' element={<WarnedRiskPartners></WarnedRiskPartners>}></Route>
-                  <Route path='/to-terminated-risk-partners' element={<ToTerminatedRiskPartners></ToTerminatedRiskPartners>}></Route>
-                  <Route path='/delivery-confirm' element={<DeliveryConfirms></DeliveryConfirms>}></Route>
-                  <Route path='/tomorrow-partners' element={<TomorrowPartners></TomorrowPartners>}></Route>
-                  <Route path='/today-partners' element={<TodayPartners></TodayPartners>}></Route>
-                  <Route path='/warning-notices' element={<WarningNotices></WarningNotices>}></Route>
-
-                  <Route path='/quick-quotations' element={<QuickQuotations></QuickQuotations>}></Route>
-                  <Route path='/quotations' element={<Quotations></Quotations>}></Route>
-
-                  <Route path='/trade-accounts' element={<TradeAccounts></TradeAccounts>}></Route>
-
-                  <Route path='/ledger-accounts' element={<LedgerAccounts></LedgerAccounts>}></Route>
-
-                  <Route path='/accounts' element={<Accounts></Accounts>}></Route>
-                  <Route path='/accounts/accounts-receivable' element={<Receivable></Receivable>}></Route>
-                  <Route path='/accounts/accounts-payable' element={<Payable></Payable>}></Route>
-                  <Route path='/accounts/bank' element={<Bank></Bank>}></Route>
-                  <Route path='/accounts/cash' element={<Cash></Cash>}></Route>
-                  <Route path='/accounts/sales' element={<Sales></Sales>}></Route>
-                  <Route path='/accounts/capital' element={<Capital></Capital>}></Route>
-                  <Route path='/accounts/expense' element={<Expense></Expense>}></Route>
-                  <Route path='/accounts/add-account/:type' element={<AddAccount></AddAccount>}></Route>
-                  <Route path='/accounts/update/:type/:uuid' element={<UpdateAccount></UpdateAccount>}></Route>
-
-                  <Route path='/invoices' element={<Invoices></Invoices>}></Route>
-                  <Route path='/invoices/add-invoice/:type' element={<AddInvoice></AddInvoice>}></Route>
-                  <Route path='/invoices/update/:type/:uuid' element={<UpdateInvoice></UpdateInvoice>}></Route>
-
-                  <Route path='/payments' element={<Payments></Payments>}></Route>
-                  <Route path='/payments/add-payment/:type' element={<AddPayment></AddPayment>}></Route>
-                  <Route path='/payments/update/:type/:uuid' element={<UpdatePayment></UpdatePayment>}></Route>
-
-                  <Route path='/banka-hareketleri' element={<BankaHareketleri></BankaHareketleri>}></Route>
-                  <Route path='/banka-tahsilatlari' element={<BankaTahsilatlari></BankaTahsilatlari>}></Route>
-                  <Route path='/banka--tahsilatlari-odoo' element={<BankaTahsilatlariOdoo></BankaTahsilatlariOdoo>}></Route>
-
-                  <Route path='auth' element={<Dashboard></Dashboard>}>
-                    <Route path='login'></Route>
-                    <Route path='register'></Route>
-                  </Route>
-
+                <Route path='/categories' element={<Categories></Categories>}>
+                  <Route path='add-category' element={<AddCategory></AddCategory>}></Route>
+                  <Route path='update/:uuid' element={<UpdateCategory></UpdateCategory>}></Route>
                 </Route>
 
-                <Route path='*' element={<WrongPath></WrongPath>}></Route>
+                <Route path='/products/update/:uuid' element={<UpdateCategory></UpdateCategory>}></Route>
 
-              </Routes>
-            </>
+                <Route path='/contracts' element={<Contracts></Contracts>}></Route>
+                <Route path='/contract-payments' element={<ContractPayments></ContractPayments>}></Route>
+
+                <Route path='/leases' element={<Leases></Leases>}></Route>
+                <Route path='/leasing/add-lease' element={<AddLease></AddLease>}></Route>
+                <Route path='/leasing/update/:uuid/:contract_code' element={<UpdateLease></UpdateLease>}></Route>
+                <Route path='/installments' element={<Installment></Installment>}></Route>
+                <Route path='/collections' element={<Collections></Collections>}></Route>
+
+                <Route path='/overdue-leases' element={<OverdueLeases></OverdueLeases>}></Route>
+                <Route path='/risk-partners' element={<RiskPartners></RiskPartners>}></Route>
+                <Route path='/kdv-risk-partners' element={<RiskPartnersKDV></RiskPartnersKDV>}></Route>
+                <Route path='/to-warned-risk-partners' element={<ToWarnedRiskPartners></ToWarnedRiskPartners>}></Route>
+                <Route path='/warned-risk-partners' element={<WarnedRiskPartners></WarnedRiskPartners>}></Route>
+                <Route path='/to-terminated-risk-partners' element={<ToTerminatedRiskPartners></ToTerminatedRiskPartners>}></Route>
+                <Route path='/delivery-confirm' element={<DeliveryConfirms></DeliveryConfirms>}></Route>
+                <Route path='/tomorrow-partners' element={<TomorrowPartners></TomorrowPartners>}></Route>
+                <Route path='/today-partners' element={<TodayPartners></TodayPartners>}></Route>
+                <Route path='/warning-notices' element={<WarningNotices></WarningNotices>}></Route>
+
+                <Route path='/quick-quotations' element={<QuickQuotations></QuickQuotations>}></Route>
+                <Route path='/quotations' element={<Quotations></Quotations>}></Route>
+
+                <Route path='/trade-accounts' element={<TradeAccounts></TradeAccounts>}></Route>
+
+                <Route path='/ledger-accounts' element={<LedgerAccounts></LedgerAccounts>}></Route>
+
+                <Route path='/accounts' element={<Accounts></Accounts>}></Route>
+                <Route path='/accounts/accounts-receivable' element={<Receivable></Receivable>}></Route>
+                <Route path='/accounts/accounts-payable' element={<Payable></Payable>}></Route>
+                <Route path='/accounts/bank' element={<Bank></Bank>}></Route>
+                <Route path='/accounts/cash' element={<Cash></Cash>}></Route>
+                <Route path='/accounts/sales' element={<Sales></Sales>}></Route>
+                <Route path='/accounts/capital' element={<Capital></Capital>}></Route>
+                <Route path='/accounts/expense' element={<Expense></Expense>}></Route>
+                <Route path='/accounts/add-account/:type' element={<AddAccount></AddAccount>}></Route>
+                <Route path='/accounts/update/:type/:uuid' element={<UpdateAccount></UpdateAccount>}></Route>
+
+                <Route path='/invoices' element={<Invoices></Invoices>}></Route>
+                <Route path='/invoices/add-invoice/:type' element={<AddInvoice></AddInvoice>}></Route>
+                <Route path='/invoices/update/:type/:uuid' element={<UpdateInvoice></UpdateInvoice>}></Route>
+
+                <Route path='/payments' element={<Payments></Payments>}></Route>
+                <Route path='/payments/add-payment/:type' element={<AddPayment></AddPayment>}></Route>
+                <Route path='/payments/update/:type/:uuid' element={<UpdatePayment></UpdatePayment>}></Route>
+
+                <Route path='/banka-hareketleri' element={<BankaHareketleri></BankaHareketleri>}></Route>
+                <Route path='/banka-tahsilatlari' element={<BankaTahsilatlari></BankaTahsilatlari>}></Route>
+                <Route path='/banka--tahsilatlari-odoo' element={<BankaTahsilatlariOdoo></BankaTahsilatlariOdoo>}></Route>
+
+                <Route path='auth' element={<Dashboard></Dashboard>}>
+                  <Route path='login'></Route>
+                  <Route path='register'></Route>
+                </Route>
+
+              </Route>
+
+              <Route path='*' element={<WrongPath></WrongPath>}></Route>
+
+            </Routes>
+          </>
           :
-            <>
-              <Routes>
+          <>
+            <Routes>
 
-                <Route path='/' element={<Landing></Landing>}>
-                  <Route index element={<Home></Home>}></Route>
-                  <Route path='auth' element={<Auth></Auth>}>
-                    <Route path='login' element={<Login></Login>}></Route>
-                    <Route path='register' element={<Register></Register>}></Route>
-                    <Route path='forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
-                  </Route>
+              <Route path='/' element={<Landing></Landing>}>
+                <Route index element={<Home></Home>}></Route>
+                <Route path='auth' element={<Auth></Auth>}>
+                  <Route path='login' element={<Login></Login>}></Route>
+                  <Route path='register' element={<Register></Register>}></Route>
+                  <Route path='forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
                 </Route>
+              </Route>
 
-                <Route path='*' element={<WrongPath></WrongPath>}></Route>
+              <Route path='*' element={<WrongPath></WrongPath>}></Route>
 
-              </Routes>
-            </>
-          }
+            </Routes>
+          </>
+        }
 
-        </div>
-      
+      </div>
+
     </ThemeProvider>
-    
+
   );
 }
 
