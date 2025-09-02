96a9ce5fe6e3974a06e6955a460c7f9f99e0c0b2
korayzorlu <korayzorllu@gmail.com>
2025-09-02 10:14:12 +0300

    #20250902 fix: fixed global settings

--- src/features/finance/pages/BankAccountTransactions.js
+++ src/features/finance/pages/BankAccountTransactions.js
@@ -2,11 +2,13 @@ import { useGridApiRef } from '@mui/x-data-grid';
 import React, { useEffect, useRef, useState, useTransition } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import PanelContent from '../../../component/panel/PanelContent';
-import { Grid } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import ListTable from '../../../component/table/ListTable';
 import CustomTableButton from '../../../component/table/CustomTableButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { fetchBankAccountTransactions, setBankAccountTransactionsParams } from '../../../store/slices/finance/bankAccountTransactionSlice';
+import { parseDate } from '../../../utils/stirngUtils';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 function BankAccountTransactions() {
     const {user} = useSelector((store) => store.auth);
@@ -29,11 +31,22 @@ function BankAccountTransactions() {
     }, [activeCompany,bankAccountTransactionsParams,dispatch]);
 
     const columns = [
-        { field: 'TransactionDate', headerName: 'Tarih', width: 180 },
+        { field: 'TransactionDate', headerName: 'Tarih', width: 160, sortComparator: (a, b) => parseDate(a) - parseDate(b) },
         { field: 'ExplanationField', headerName: 'Açıklama', width: 520 },
         { field: 'Amount', headerName: 'Tutar', width: 140, type: 'number', renderHeaderFilter: () => null },
         { field: 'BankName', headerName: 'Banka', width: 140 },
         { field: 'OwnerAccountNo', headerName: 'Banka Hesabı', width: 240 },
+        { field: 'tahsilat', headerName: 'Tahsilat İşleme', width: 240, renderCell: (params) => (
+                params.row.Debit === "+"
+                ?
+                    <Button key={params.row.TransactionId} variant='contained' color="success" endIcon={<ArrowRightAltIcon />} size='small'>
+                        Tahsilata Gönder
+                    </Button>
+                :
+                    null
+                
+            ) 
+        },
     ]
 
     return (
@@ -57,6 +70,16 @@ function BankAccountTransactions() {
                 setParams={(value) => dispatch(setBankAccountTransactionsParams(value))}
                 headerFilters={true}
                 noDownloadButton
+                initialState={{
+                    sorting: {
+                        sortModel: [
+                        {
+                            field: 'TransactionDate',
+                            sort: 'desc',
+                        },
+                        ],
+                    },
+                }}
                 />
             </Grid>
         </PanelContent>
--- src/utils/stirngUtils.js
+++ src/utils/stirngUtils.js
@@ -6,3 +6,10 @@ export function capitalize(word) {
 export const turkishSortComparator = (a, b) => {
   return String(a).localeCompare(String(b), "tr", { sensitivity: "base" });
 };
+
+export function parseDate(dateStr) {
+  const [datePart, timePart] = dateStr.split(" ");
+  const [day, month, year] = datePart.split(".").map(Number);
+  const [hours, minutes] = timePart.split(":").map(Number);
+  return new Date(year, month - 1, day, hours, minutes);
+}
