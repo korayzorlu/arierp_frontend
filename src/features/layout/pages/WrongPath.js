03d3f27d9c6e66cb5a5403cfcd4e3a89099503f2
korayzorlu <korayzorllu@gmail.com>
2025-12-01 14:45:15 +0300

    #251201 fix: fixed global settings

--- src/features/layout/pages/WrongPath.js
+++ src/features/layout/pages/WrongPath.js
@@ -1,27 +1,40 @@
+import { Grid, Stack, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
+import { ReactComponent as ErrorIcon } from "images/icons/global/error-404-2.svg";
 
 function WrongPath() {
-    const {theme} = useSelector((store) => store.auth);
+    const {theme,dark} = useSelector((store) => store.auth);
 
-    return ( 
-        <div className="row w-100 vh-100 g-0">
-            <div className="col-md-12 text-center d-flex flex-column justify-content-center align-items-center">
-
-                <div className="row mb-5">
-                    <div className="col-md-12 text-center">
-                        <img src={require(`../../../images/logo/${theme}/ari-logo-full.png`)} height="40" alt=""/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <img src={require(`../../../images/icons/global/error-404.png`)} height="400" alt=""/>
-                    </div>
-                </div>
-                
-                
-
-            </div>
-        </div>
+    return (
+        <Stack sx={{minHeight: "100vh",justifyContent: "center",alignItems: "center",}}>
+            <Stack spacing={1} alignItems="center">
+                {/* <Typography className="archivo-black-regular" variant="h3" sx={{color: dark ? "#fff" : "#000",textAlign: "center",}}>
+                    ARINET
+                </Typography> */}
+                <Typography
+                className="archivo-black-regular"
+                variant="h4"
+                sx={{
+                    color: dark ? "#fff" : "#000",
+                    position: "absolute",
+                    top: 24,
+                    left: 24,
+                    textAlign: "left",
+                    zIndex: 1,
+                }}
+                >
+                    ARINET
+                </Typography>
+                <Typography sx={{fontSize: "8rem", color: dark ? "metallicgold.main" : "ari.main",textAlign: "center",fontWeight: "bold",}}>
+                    404
+                </Typography>
+                {/* <ErrorIcon height="400" style={{ display: "block", margin: "0 auto" }}/> */}
+                <Typography variant="h6" sx={{color: dark ? "#fff" : "#000",textAlign: "center",fontWeight: "bold",}}>
+                    Sayfa bulunamadı!
+                </Typography>
+                {/* <img src={require("../../../images/icons/global/error-404.png")} height="400" alt="" style={{ display: "block", margin: "0 auto" }}/> */}
+            </Stack>
+        </Stack>
     );
 }
 
