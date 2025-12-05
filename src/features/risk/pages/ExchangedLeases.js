8f26294737425c18d2e314d6e514fed3a85587f5
korayzorlu <korayzorllu@gmail.com>
2025-12-05 08:37:22 +0300

    #251205 fix: fixed global settings

--- src/features/risk/pages/ExchangedLeases.js
+++ src/features/risk/pages/ExchangedLeases.js
@@ -71,12 +71,6 @@ function ExchangedLeases() {
         },
         { field: 'partner_tc', headerName: 'Müşteri TC/VKN', width:160 },
         { field: 'activation_date', headerName: 'Aktifleştirme Tarihi', renderHeaderFilter: () => null },
-        { field: 'overdue_amount', headerName: 'Gecikme Tutarı', width:120, type: 'number', renderHeaderFilter: () => null, cellClassName: (params) => {
-                return params.value > 0 ? 'bg-red' : '';
-            },
-            renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.value)
-        },
-        { field: 'currency', headerName: 'PB', width:90 },
         { field: 'overdue_days', headerName: 'Gecikme Süresi', width:120, type: 'number', renderHeaderFilter: () => null, renderCell: (params) => (
                 params.row.overdue_amount > 0
                 ?
@@ -96,12 +90,31 @@ function ExchangedLeases() {
         { field: 'amount_paid_to_date_locale', headerName: 'Ödenen Yerel', width:120, type: 'number', renderHeaderFilter: () => null,
             renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.amount_paid_to_date_locale)
         },
+        { field: 'overdue_amount', headerName: 'Geciken Yerel', width:120, type: 'number', renderHeaderFilter: () => null, 
+            renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.value)
+        },
+         { field: 'overdue_amount_usd', headerName: 'Geciken USD', width:120, type: 'number', renderHeaderFilter: () => null, cellClassName: (params) => {
+                return params.row.exchanged_amounts.overdue_amount_usd > 0 ? 'bg-red' : '';
+            },
+            renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.overdue_amount_usd)
+        },
         { field: 'amount_due_to_date_usd', headerName: 'Ödenmesi Gereken USD', width:180, type: 'number', renderHeaderFilter: () => null,
             renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.amount_due_to_date_usd)
         },
         { field: 'amount_paid_to_date_usd', headerName: 'Ödenen USD', width:120, type: 'number', renderHeaderFilter: () => null,
             renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.amount_paid_to_date_usd)
         },
+        { field: 'due_overdue_amount_usd', headerName: 'Geciken ÖDenmesi Gereken USD', width:200, type: 'number', renderHeaderFilter: () => null, cellClassName: (params) => {
+                return params.row.exchanged_amounts.overdue_amount_usd > 0 ? 'bg-red' : '';
+            },
+            renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.due_overdue_amount_usd)
+        },
+        { field: 'kur_kaybi', headerName: 'Kurdan Kayıp', width:160, type: 'number', renderHeaderFilter: () => null, cellClassName: (params) => {
+                return params.row.exchanged_amounts.overdue_amount_usd > 0 ? 'bg-dark-red' : '';
+            },
+            renderCell: (params) =>  new Intl.NumberFormat('tr-TR', { minimumFractionDigits: 2,maximumFractionDigits: 2,}).format(params.row.exchanged_amounts.kur_kaybi)
+        },
+        { field: 'currency', headerName: 'PB', width:90 },
         { field: 'status', headerName: 'Alt Statü', width:120 },
         { field: 'lease_status', headerName: 'Statü', width:120 },
     ]
