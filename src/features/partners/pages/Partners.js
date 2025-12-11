d2060604487ffe5ae22c334a982c3f4efe7677e2
korayzorlu <korayzorllu@gmail.com>
2025-12-11 14:45:12 +0300

    #251211 fix: fixed global settings

--- src/features/partners/pages/Partners.js
+++ src/features/partners/pages/Partners.js
@@ -200,6 +200,7 @@ function Partners() {
                     
                 </>
             }
+            noDownloadButton
             onRowSelectionModelChange={(newRowSelectionModel) => {
                 setSelectedItems(newRowSelectionModel);
             }}
