9390b70417e309b607c5a09703e9f7db69954a6c
korayzorlu <korayzorllu@gmail.com>
2026-09-08 14:40:05 +0300

    #2600908 fix: fixed global settings

--- src/features/operation/pages/UntitleDeedLeases.js
+++ src/features/operation/pages/UntitleDeedLeases.js
@@ -117,7 +117,7 @@ function UntitleDeedLeases() {
         },
         { field: 'block', headerName: 'Blok' },
         { field: 'unit', headerName: 'Bağımsız Bölüm' },
-        { field: 'bbsn', headerName: 'BBSN', width:140 },
+        { field: 'ari_bbsn', headerName: 'BBSN', width:140 },
         //{ field: 'vade', headerName: 'Vade', type: 'number' },
         //{ field: 'vat', headerName: 'KDV(%)', type: 'number' },
         //{ field: 'musteri_baz_maliyet', headerName: 'Müşteri Baz Maliyet', type: 'number'},
